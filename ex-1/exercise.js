3efacf42abdf92660ecf5552a33f2ef10b5ed52e
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-26 11:38:27 +0000

    ex 1 done for fuctions

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,12 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+
+function story(noun, verb, adverb, adjective){
+    
+    var noun = prompt ("insert a name");
+    var verb = prompt("write a verb");
+    var adverb = prompt("insert an adverb");
+    var adjective = prompt("insert the adjective");
+
+    window.alert(noun+" "+verb+" "+adverb+" everyday he is a "+adjective+" person.");
+
+}
+story()
