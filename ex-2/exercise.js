578eb9f6a90176610979d02cc1e522acfb6aad4d
Muhammad rRehman <muhammadislamian@gmail.com>
2021-02-26 12:41:30 +0000

    ex2 done for function

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,8 @@
-// write your JS here
-// remember to always test in the browser
-// to see if everything works correctly
+function furtune(kids, partner, geoloc, job){
+    var kids=prompt("enter number of kids");
+    var partner=prompt("enter your partners name");
+    var geoloc=prompt("enter location");
+    var job=prompt("enter your job");
+window.alert("You will be a "+job+" in "+geoloc+" and married to "+partner+" with "+kids+" kids.");
+}
+furtune()
